eed5bdca229d9adf9ad4801ecb379962a4f6d742
bangjinhyuk <jinhyuk0103@gmail.com>
2022-01-06 22:27:44 +0900

    test

--- Sol/src/BOJ/BOJ_2589.java
+++ Sol/src/BOJ/BOJ_2589.java
@@ -1,10 +1,13 @@
 package BOJ;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Created by bangjinhyuk on 2022/01/06.
  */
 public class BOJ_2589 {
     public static void main(String[] args) {
-        
+        var br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
